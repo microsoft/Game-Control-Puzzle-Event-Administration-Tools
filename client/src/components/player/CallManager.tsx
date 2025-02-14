9aaf9fc5f581b5c4a853b1c96f276c3a01fb99f7
Shane Lile <shanel@microsoft.com>
2025-02-13 18:28:22 -0800

    Some easy updates

--- client/src/components/player/CallManager.tsx
+++ client/src/components/player/CallManager.tsx
@@ -1,25 +1,25 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import { Card, Button, Row} from 'react-bootstrap';
+import { Card, Button, Row } from 'react-bootstrap';
 import { FaPuzzlePiece, FaQuestionCircle } from 'react-icons/fa';
 
 import { getAllPlayerPuzzles, getPointsNameSetting, getShowCallManagerSetting } from 'modules';
-import { usePlayerCalls } from "modules/player/calls";
+import { usePlayerCalls } from 'modules/player/calls';
 import { fetchPlayerClues } from 'modules/player/clues/service';
 import { useTeamData } from 'modules/player/team/hooks';
 import { PlayerActiveCall } from './PlayerActiveCall';
+import { PlayerClue } from 'modules/player';
 
 export const CallManager = () => {
-    const { playerCalls, updateCall} = usePlayerCalls();
+    const { playerCalls, updateCall } = usePlayerCalls();
     const { teamModule } = useTeamData();
     const allPuzzles = useSelector(getAllPlayerPuzzles);
     const pointsName = useSelector(getPointsNameSetting);
 
     const showCallManager = useSelector(getShowCallManagerSetting);
 
-    const unsolvedPuzzles = allPuzzles.filter(clue =>
-        clue.submissionTime === null && clue.submittableType.trim() !== 'Plot');
+    const unsolvedPuzzles = allPuzzles.filter((clue: PlayerClue) => clue.submissionTime === null && clue.submittableType.trim() !== 'Plot');
 
     useEffect(() => {
         fetchPlayerClues();
@@ -27,35 +27,34 @@ export const CallManager = () => {
 
     if (showCallManager) {
         if (playerCalls.data && playerCalls.data.length > 0) {
-            return (              
+            return (
                 <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'stretch' }}>
                     <Card className="playerActiveCallCard">
                         <Card.Header>Active Call with Game Control</Card.Header>
                         <Card.Body>
-                        <PlayerActiveCall currentCall={playerCalls.data[0]} onUpdate={updateCall} />
+                            <PlayerActiveCall currentCall={playerCalls.data[0]} onUpdate={updateCall} />
                         </Card.Body>
                     </Card>
-                </div> 
-                
+                </div>
             );
         } else {
             return (
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    { (!unsolvedPuzzles || unsolvedPuzzles.length === 0) &&
-                        <Button variant="outline-info"
-                                className="playerCallButton"
-                                onClick={() => updateCall({ callType: "TeamFree" })}>
-                            <div className="playerCallButtonIcon"><FaPuzzlePiece/></div>
+                    {(!unsolvedPuzzles || unsolvedPuzzles.length === 0) && (
+                        <Button variant="outline-info" className="playerCallButton" onClick={() => updateCall({ callType: 'TeamFree' })}>
+                            <div className="playerCallButtonIcon">
+                                <FaPuzzlePiece />
+                            </div>
                             <div>Request New Puzzles</div>
                         </Button>
-                    }
+                    )}
                     {
-                    <Button variant="outline-warning"
-                            className="playerCallButton"
-                            onClick={() => updateCall({ callType: "TeamHelp" })}>
-                        <div className="playerCallButtonIcon"><FaQuestionCircle/></div>
-                        <div>Request Help</div>
-                    </Button>
+                        <Button variant="outline-warning" className="playerCallButton" onClick={() => updateCall({ callType: 'TeamHelp' })}>
+                            <div className="playerCallButtonIcon">
+                                <FaQuestionCircle />
+                            </div>
+                            <div>Request Help</div>
+                        </Button>
                     }
                     {/*
                     <Card style={{ width: '175px', margin: "10px" }}>
@@ -66,9 +65,7 @@ export const CallManager = () => {
                 </div>
             );
         }
+    } else {
+        return <div></div>;
     }
-    else
-    {
-        return (<div></div>);
-    }
-}
+};
