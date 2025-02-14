88334ece36d45ee8a14c1d1a075cee7f8c80b49c
Shane Lile <shanel@microsoft.com>
2025-02-13 18:28:22 -0800

    Some easy updates

---
+++ client/src/components/errors/Forbidden.tsx
@@ -0,0 +1,10 @@
+const Forbidden = () => {
+    return (
+        <div>
+            <h3>Forbidden</h3>
+            <p>You do not have access to this resource</p>
+        </div>
+    );
+};
+
+export default Forbidden;
