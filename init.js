06a620d8690323da44cfeef51fb4b93f0ae3c40f
Danny Paez <danny.paez@lucidoc.com>
2026-05-17 11:35:08 -0700

    Sanitizing REACT_USER and HOST_DIR env variables

--- init.js
+++ init.js
@@ -187,8 +187,8 @@ function main() {
                     }
 
                     if (process.platform === 'linux') {
-                        env.push(`REACT_USER=${process.getuid()}:${process.getgid()}`);
-                        env.push(`HOST_DIR=${process.cwd()}`);
+                        env.push(`REACT_USER="${process.getuid()}:${process.getgid()}"`);
+                        env.push(`HOST_DIR="${process.cwd()}"`);
                     }
 
                     const origin = "http://" + answers.serverFqdn;
