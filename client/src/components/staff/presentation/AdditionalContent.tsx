0417ad6ee9c96d74782642d15a7aa64599b51567
Alex Marcellus <almarcel@microsoft.com>
2026-07-25 08:51:19 -0700

    Map map latitude longitude pins

--- client/src/components/staff/presentation/AdditionalContent.tsx
+++ client/src/components/staff/presentation/AdditionalContent.tsx
@@ -29,7 +29,7 @@ const HtmlContent = ({ content }: ContentProps) => {
 
 const LocationContent = ({ content }: ContentProps) => {
     // Create Bing Maps URL with the location coordinates
-    const bingMapsUrlLatLong = `https://www.bing.com/maps/default.aspx?lvl=18&cp=${content.latitude}~${content.longitude}`;
+    const bingMapsUrlLatLong = `https://www.bing.com/maps?lvl=18&sp=point.${content.latitude}_${content.longitude}_Location`;
     const bingMapsUrlAddress = `https://www.bing.com/maps/default.aspx?&where1=${content.address}`;
 
     return (
